bc7e404f15bdc2719a230c9324953ddbc190457b
Dan Edens <Danedens31@gmail.com>
2022-12-14 18:00:36 -0800

    Updates __init__

--- sitecheck/__init__.py
+++ sitecheck/__init__.py
@@ -1,18 +1,20 @@
 """
-    init file for Join-CLi 
+    init file for Join-CLi
 """
 # __name__ = 'join'
-
 
 import os
 import sys
 import datetime
 
+# Set the current working directory as the ROOT_DIR environment variable
+os.environ['ROOT_DIR'] = os.getcwd()
 
-today = datetime.datetime.utcnow()
+# Set the filedate and Nowdate environment variables using the current date and time
+filedate = datetime.datetime.utcnow().strftime("%Y-%m-%d")
+os.environ['filedate'] = filedate
+nowdate = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+os.environ['Nowdate'] = nowdate
 
-os.environ['ROOT_DIR'] = os.path.dirname(os.path.abspath(__file__))
-os.environ['filedate'] = today.strftime("%Y-%m-%d")
-os.environ['Nowdate'] = today.strftime("%Y-%m-%d %H:%M:%S")
-print(os.environ['ROOT_DIR'])
+# Add the current working directory to the list of directories to search for modules
 sys.path.insert(0, os.environ['ROOT_DIR'])
